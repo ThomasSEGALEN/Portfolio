9d42957d816ad85787e7592820d192ebcfdf0d39
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2021-05-20 10:44:22 +0200

    index redirection

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Labyrinthe PHP</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="labyrinth_game_menu.css">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+</head>
+
+<body>
+
+    <?php
+    header('Location: ./home.html');
+    ?>
+
+</body>
+
+</html>
